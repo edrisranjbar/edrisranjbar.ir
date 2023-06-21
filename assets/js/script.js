a28138459baf0a9e0c31a404dff8a599869a05c9
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-06-21 03:42:08 +0330

    fix #8

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     let splide = new Splide('.splide', {
+        type: 'loop',
         perPage: 'auto',
         direction: 'rtl',
         gap: '16px',
         arrows: false,
         pagination: false,
         perMove: 1,
+        snap: true,
+        omitEnd: true,
     });
     splide.mount();
 });
 document.addEventListener('DOMContentLoaded', function () {
     let splide = new Splide('.blog-right.splide', {
+        type: 'loop',
         perPage: 'auto',
         direction: 'rtl',
         gap: '16px',
         arrows: false,
         pagination: false,
         perMove: 1,
+        snap: true,
+        omitEnd: true,
     });
     splide.mount();
 });
