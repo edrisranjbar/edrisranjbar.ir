0068bacebd977ce896302c48c4e0d9abd0dc838b
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-08-18 23:58:52 +0330

    mount splide if neede only

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,28 +3,32 @@ document.addEventListener('DOMContentLoaded', function () {
         let tutorialsSliderInSidebar = new Splide('.tutorialsInSidebar', { pagination: true, arrows: true });
         tutorialsSliderInSidebar.mount();
     }
-    let splide = new Splide('.splide', {
-        type: 'loop',
-        perPage: 'auto',
-        direction: 'rtl',
-        gap: '16px',
-        arrows: false,
-        pagination: false,
-        perMove: 1,
-        snap: true,
-        omitEnd: true,
-    });
-    splide.mount();
-    let splide2 = new Splide('.blog-right.splide', {
-        type: 'loop',
-        perPage: 'auto',
-        direction: 'rtl',
-        gap: '16px',
-        arrows: false,
-        pagination: false,
-        perMove: 1,
-        snap: true,
-        omitEnd: true,
-    });
-    splide2.mount();
+    if (document.querySelectorAll('.splide').length > 0) {
+        let splide = new Splide('.splide', {
+            type: 'loop',
+            perPage: 'auto',
+            direction: 'rtl',
+            gap: '16px',
+            arrows: false,
+            pagination: false,
+            perMove: 1,
+            snap: true,
+            omitEnd: true,
+        });
+        splide.mount();
+    }
+    if (document.querySelectorAll('.blog-right.splide').length > 0) {
+        let splide2 = new Splide('.blog-right.splide', {
+            type: 'loop',
+            perPage: 'auto',
+            direction: 'rtl',
+            gap: '16px',
+            arrows: false,
+            pagination: false,
+            perMove: 1,
+            snap: true,
+            omitEnd: true,
+        });
+        splide2.mount();
+    }
 });
