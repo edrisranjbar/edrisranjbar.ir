16b61d25ad018f606d9c6044e87d8baaa9c88702
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-11-17 01:47:58 +0330

    fix humb. icon color

--- resources/views/templates/main-menu.blade.php
+++ resources/views/templates/main-menu.blade.php
@@ -3,8 +3,8 @@
         <h1 class="h3 text-primary">ادریس رنجبر</h1>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background: none; border:none;">
+        <i class="fa fa-bars text-light"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
