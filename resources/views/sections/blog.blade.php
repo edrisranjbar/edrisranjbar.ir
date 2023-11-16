1cadcafcb0a6d262443777310616b6a30e2a2a3a
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-11-17 02:55:01 +0330

    update homepage UI

--- resources/views/sections/blog.blade.php
+++ resources/views/sections/blog.blade.php
@@ -4,6 +4,10 @@
         <div class="breaker"></div>
         <p class="blog-section-description">این یک متن تستیه، این یک متن تستیه، این یک متن تستیه، این یک
             متن تستیه، این یک متن تستیه...</p>
+        <a href="#" class="btn btn-lg btn-outline-primary text-light d-inline-block btn-w-icon">
+            <i class="fa fa-eye me-1"></i>
+            مشاهده همه
+        </a>
     </div>
     <div class="col-12 col-xl-6 splide blog-right" role="group" aria-label="اسلایدر نوشته ها">
         <div class="splide__track">
@@ -16,7 +20,10 @@
                     </a>
                     <h3 class="post-title">{{ $post->title }}</h3>
                     <div class="d-flex post-meta">
-                        <a href="{{ $post->link }}" class="btn btn-sm btn-primary">مشاهده</a>
+                        <a href="{{ $post->link }}" class="btn btn-sm text-light btn-primary btn-w-icon">
+                            <i class="fa fa-solid fa-eye me-1"></i>
+                            مشاهده
+                        </a>
                     </div>
                 </li>
                 @empty
