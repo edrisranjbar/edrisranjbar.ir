b2fb5b80859a92033d20510f223741204aab0ca7
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-11-17 22:15:17 +0330

    fix issues related to tutorials admin page

--- resources/views/admin/tutorials/createOrEdit.blade.php
+++ resources/views/admin/tutorials/createOrEdit.blade.php
@@ -10,15 +10,19 @@
     <h1 class="h3 mb-3 fw-normal text-right">
         {{ !isset($tutorial) ? 'ایجاد آموزش جدید' : $tutorial->title }}
     </h1>
-    <form class="w-100 p-3 bg-white rounded shadow-sm border" @if(isset($tutorial))
-        action="{{ route('tutorials.update', ['tutorial'=> $tutorial->id]) }}" @else
-        action="{{ route('tutorials.store') }}" @endif method="POST" enctype="multipart/form-data">
+    <form class="w-100 p-3 bg-white rounded shadow-sm border"
+        @if(isset($tutorial))
+        action="{{ route('tutorials.update', ['tutorial'=> $tutorial->id]) }}"
+        @else
+        action="{{ route('tutorials.store') }}"
+        @endif
+        method="POST" enctype="multipart/form-data">
         @csrf
         @if(isset($tutorial))
         @method('patch')
         @endif
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-12 col-lg-8">
                 <div class="mb-3">
                     <label for="title" class="form-label">عنوان</label>
                     <input type="text" name="title" id="title" class="form-control"
@@ -37,19 +41,31 @@
                     @enderror
                 </div>
 
-                <div class="d-flex flex-wrap mt-2">
-                    <button type="submit" class="btn btn-w-icon btn-primary mt-2 me-2">
-                        <i class="fa-solid fa-fw fa-check me-1"></i>
-                        ذخیرۀ تغییرات
+                @if(isset($form))
+                <div class="d-flex flex-wrap">
+                    <button class="btn btn-w-icon btn-primary mt-2 me-2" type="submit">
+                        <i class="fa-solid fa-fw fa-edit me-1"></i>
+                        ویرایش
                     </button>
-                    <button type="button" class="btn btn-w-icon btn-outline-danger mt-2 me-2" data-bs-toggle="modal"
-                        data-bs-target="#deleteTutorialModal">
-                        <i class="fa-solid fa-fw fa-trash me-1"></i>
-                        حذف
-                    </button>
+                    <a class="btn bt-w-icon btn-outline-secondary mt-2 me-2" href="{{ route('form4.index') }}">
+                        برگشت
+                    </a>
                 </div>
+                @else
+                <div class="d-flex flex-wrap">
+                    <button class="btn btn-w-icon btn-primary me-2" type="submit">
+                        <i class="fa-solid fa-fw fa-check me-1"></i>
+                        ثبت
+                    </button>
+                    <a class="btn btn-w-icon btn-outline-primary me-2">
+                        <i class="fa-solid fa-fw fa-list-check me-1"></i>
+                        ثبت و ساخت جدید
+                    </a>
+                </div>
+                @endif
+
             </div>
-            <div class="col-lg-4">
+            <div class="col-12 col-lg-4">
                 <div class="w-100 p-3 bg-white rounded shadow-sm border">
                     <div class="mb-3">
                         <label for="slug" class="form-label">شناسه یکتا</label>
@@ -58,6 +74,14 @@
                         @error('slug')
                         <p class="text-danger">{{ $message }}</p>
                         @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="duration" class="form-label">مدت دوره</label>
+                        <input type="number" name="duration" id="duration" class="form-control" required min="1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">هزینه شرکت در دوره</label>
+                        <input type="number" name="price" id="price" class="form-control" required min="0" value="0">
                     </div>
                     <div class="mb-3">
                         <label for="status" class="form-label">وضعیت</label>
@@ -76,7 +100,19 @@
                         <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
-                    <!-- Other form fields with similar changes -->
+                    <div class="mb-3">
+                        <label for="tutor" class="form-label">مدرس</label>
+                        <select name="tutor" id="tutor" class="form-select" required>
+                            @foreach ($tutors as $tutor)
+                            <option value="{{ $tutor->id }}" @if(isset($tutorial) && $tutorial->tutor == $tutor->id) selected @endif>
+                                {{ $tutor->fullName }}
+                            </option>
+                            @endforeach
+                        </select>
+                        @error('tutor')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
                     <div class="mb-3">
                         <label for="thumbnail" class="form-label">تصویر بند انگشتی</label>
                         <input type="file" name="thumbnail" id="thumbnail" class="form-control" @if(!isset($tutorial))
