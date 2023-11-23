ad7169cb22dd5d62b2f66f0962010ad651ade2a4
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-11-24 01:35:52 +0330

    update method added to lesson controller

--- app/Http/Controllers/lessonController.php
+++ app/Http/Controllers/lessonController.php
@@ -65,6 +65,44 @@ class lessonController extends Controller
         return redirect()->route('lessons.index')->with('success', 'درس با موفقیت اضافه شد.');
     }
 
+    public function update(Request $request, int $id)
+    {
+        $lesson = Lesson::findOrFail($id);
+        $validationRules = [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'seoTitle' => 'required|string|max:255',
+            'seoDescription' => 'nullable|string',
+            'slug' => 'nullable|string|max:50',
+            'order_id' => 'nullable|integer|min:1|max:1000',
+            'status' => 'required|in:public,private,draft',
+            'duration' => 'required|integer|min:1',
+            'isFree' => 'nullable|in:true,false',
+            'file' => 'nullable|file|mimes:zip',
+            'video' => 'nullable|file|mimetypes:video/*',
+            'tutorial_id' => 'required|exists:tutorials,id',
+            'section_id' => 'required|exists:sections,id',
+        ];
+        $validatedData = $request->validate($validationRules);
+
+        // Handle file uploads
+        if ($request->hasFile('file')) {
+            $request->file('file')->store('public/upload');
+            $validatedData['file_path'] = $request->file('file')->hashName();
+        }
+
+        // Handle video uploads
+        if ($request->hasFile('video')) {
+            $request->file('video')->store('public/upload');
+            $validatedData['video_path'] = $request->file('video')->hashName();
+        }
+
+        $validatedData['isFree'] = boolval($validatedData['isFree'] ?? false);
+        unset($validatedData['file'], $validatedData['video']);
+        $lesson->update($validatedData);
+        return redirect()->route('lessons.index')->with('success', 'درس با موفقیت ویرایش شد.');
+    }
+
     public function destroy(int $id)
     {
         $lesson = Lesson::findOrFail($id);
