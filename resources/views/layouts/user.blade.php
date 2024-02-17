91e2310b621cb4baa86ab58d1901109a7414eaa7
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2024-02-17 12:21:13 +0330

    update user panel title

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>داشبورد - @yield('title')</title>
+    <title>پنل مهارت آموز - @yield('title')</title>
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <link href="{{ asset('assets/fonts/fontawesome-6.2.1/css/all.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/fonts.css') }}" rel="stylesheet" type="text/css">
