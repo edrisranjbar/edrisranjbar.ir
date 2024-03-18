142557b8f662c875df87a55fbf455b6860ae6a39
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2024-03-18 07:00:13 +0330

    enhance: add posts and tutorials to dynamic sitemap

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Tutorial;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class SiteMapController extends Controller
 {
     public function generate()
     {
-        return SitemapGenerator::create('https://edrisranjbar.ir')->getSitemap();
+        $siteMap = SitemapGenerator::create('https://edrisranjbar.ir')
+            ->getSitemap()
+            ->add(Url::create('/'))
+            ->add(Url::create('/blog'))
+            ->add(Url::create('/podcasts'))
+            ->add(Url::create('/tutorials'))
+            ->add(Url::create('/user/login'))
+            ->writeToFile(public_path('sitemap.xml'));
+
+        Post::all()->each(function (Post $post) use ($siteMap) {
+            $siteMap->add(Url::create($post->link));
+        });
+
+        Tutorial::all()->each(function (Tutorial $tutorial) use ($siteMap) {
+            $siteMap->add(Url::create($tutorial->link));
+        });
+
+        return $siteMap;
     }
 }
