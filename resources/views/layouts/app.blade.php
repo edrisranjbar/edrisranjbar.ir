78002b42895741c6ee724b33d1b7cedb9af22c33
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2024-03-19 07:51:50 +0330

    feature: favicon added in multi sizes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,11 @@
     <link rel="stylesheet" href="{{ asset('assets/css/fonts.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/splide.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/moovie.min.css') }}">
+    <!-- FavIcon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}">
 </head>
 
 <body class="@yield('body-class', '')">
