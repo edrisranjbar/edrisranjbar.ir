e7de965a6164b4f96b080f2ae698ee1622ee72ba
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2024-03-22 10:10:15 +0330

    comments migration added

---
+++ database/migrations/2024_03_22_063056_posts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained('users')->onDelete('set null');
+            $table->foreignId('parent_id')->nullable()->constrained('comments')->onDelete('cascade');
+            $table->unsignedBigInteger('post_id')->nullable();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->text('message');
+            $table->boolean('confirmed')->default(false);
+            $table->text('reply_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
