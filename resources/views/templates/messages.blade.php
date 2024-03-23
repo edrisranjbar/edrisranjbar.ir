429661e659193c65452fdfca3f999c0e07a0ada9
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2024-03-23 06:45:56 +0330

    enhance alerts

--- resources/views/templates/messages.blade.php
+++ resources/views/templates/messages.blade.php
@@ -1,5 +1,5 @@
 @if ($errors->any())
-<div class="alert alert-danger alert-dismissible fade show text-right d-flex align-items-center justify-content-between" role="alert">
+<div class="py-1 alert alert-danger alert-dismissible fade show text-right d-flex align-items-center justify-content-between" role="alert">
     <strong class="d-block mb-2">لطفا خطاهای زیر را رفع کرده و دوباره تلاش کنید 👇</strong>
     <ul>
         @foreach ($errors->all() as $error)
@@ -11,14 +11,14 @@
 @endif
 
 @if (session('success'))
-<div class="alert alert-success alert-dismissible fade show text-right d-flex align-items-center justify-content-between" role="alert">
+<div class="py-1 alert alert-success alert-dismissible fade show text-right d-flex align-items-center justify-content-between" role="alert">
     {{ session('success') }}
     <button style="position: unset;" type="button" class="btn-close py-3" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 @if (session('info'))
-<div class="alert alert-info alert-dismissible fade show text-right d-flex align-items-center justify-content-between" role="alert">
+<div class="py-1 alert alert-info alert-dismissible fade show text-right d-flex align-items-center justify-content-between" role="alert">
     {{ session('info') }}
     <button style="position: unset;" type="button" class="btn-close py-3" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
