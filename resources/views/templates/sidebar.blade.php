adf9968b6c44aee6803e4b7718732ba46057a181
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2025-03-06 03:47:47 +0330

    update admin sidebar

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -1,4 +1,15 @@
-<ul class="m-3 p-0 navbar-nav bg-black sidebar sidebar-dark text-right px-2" id="accordionSidebar">
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('admin') }}">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">داشبورد</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
@@ -8,46 +19,38 @@
         </a>
     </li>
 
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle collapsed" data-bs-toggle="collapse" data-bs-target="#collapsePosts"
-            aria-expanded="false" aria-controls="collapsePosts">
+    <!-- Nav Item - Posts -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePosts" aria-expanded="true"
+            aria-controls="collapsePosts">
             <i class="fas fa-fw fa-pen"></i>
             <span>نوشته ها</span>
         </a>
-        <div id="collapsePosts" class="collapse" aria-labelledby="collapsePosts" data-parent="#accordionSidebar">
-            <div class="collapse-inner">
-                <a class="collapse-item" href="{{ route('posts.index') }}">
-                    <span>لیست نوشته ها</span>
-                </a>
-                <a href="{{ route('categories.index') }}" class="collapse-item">
-                    دسته بندی ها
-                </a>
+        <div id="collapsePosts" class="collapse" aria-labelledby="headingPosts" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('posts.index') }}">لیست نوشته ها</a>
+                <a class="collapse-item" href="{{ route('categories.index') }}">دسته بندی ها</a>
             </div>
         </div>
     </li>
 
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTutorials"
-            aria-expanded="false" aria-controls="collapseTutorials">
+    <!-- Nav Item - Tutorials -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTutorials"
+            aria-expanded="true" aria-controls="collapseTutorials">
             <i class="fas fa-fw fa-book"></i>
             <span>دوره های آموزشی</span>
         </a>
-        <div id="collapseTutorials" class="collapse" aria-labelledby="collapseTutorials"
-            data-parent="#accordionSidebar">
-            <div class="collapse-inner">
-                <a class="collapse-item" href="{{ route('tutorials.index') }}">
-                    <span>لیست دوره ها</span>
-                </a>
-                <a class="collapse-item" href="{{ route('lessons.index') }}">
-                    <span>درس ها</span>
-                </a>
-                <a class="collapse-item" href="{{ route('students.index') }}">
-                    <span>دانشجویان</span>
-                </a>
+        <div id="collapseTutorials" class="collapse" aria-labelledby="headingTutorials" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('tutorials.index') }}">لیست دوره ها</a>
+                <a class="collapse-item" href="{{ route('lessons.index') }}">درس ها</a>
+                <a class="collapse-item" href="{{ route('students.index') }}">دانشجویان</a>
             </div>
         </div>
     </li>
 
+    <!-- Nav Item - Contact Forms -->
     <li class="nav-item">
         <a class="nav-link" href="{{ url('admin/contactForms') }}">
             <i class="fas fa-envelope"></i>
@@ -55,6 +58,7 @@
         </a>
     </li>
 
+    <!-- Nav Item - Comments -->
     <li class="nav-item">
         <a class="nav-link" href="{{ url('admin/comments') }}">
             <i class="fas fa-fw fa-comment"></i>
@@ -62,6 +66,7 @@
         </a>
     </li>
 
+    <!-- Nav Item - Analytics -->
     <li class="nav-item">
         <a class="nav-link" href="{{ url('admin/analytics') }}">
             <i class="fas fa-fw fa-chart-pie"></i>
@@ -69,10 +74,19 @@
         </a>
     </li>
 
+    <!-- Nav Item - Settings -->
     <li class="nav-item">
         <a class="nav-link" href="{{ url('admin/settings') }}">
             <i class="fas fa-fw fa-wrench"></i>
             <span>تنظیمات</span>
         </a>
     </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
 </ul>
