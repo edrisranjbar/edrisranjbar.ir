17705784b9120fea18304932322d81c893a5864e
Edris <edris.qeshm2@gmail.com>
2025-05-12 05:11:12 +0330

    feat: add new FontAwesome icon (faXmark) to main.js; import main.css for improved styling

--- edicodes/src/main.js
+++ edicodes/src/main.js
@@ -4,17 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Particles from 'vue3-particles'
+import './assets/main.css'
 
 /* Import FontAwesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* Import specific icons */
-import { faEnvelope, faHome, faHeart, faBook, faServer, faDatabase, faDiagramProject, faCode, faBolt, faShield, faPhone, faLocationDot, faPaperPlane, faCreditCard, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faHeart, faBook, faServer, faDatabase, faDiagramProject, faCode, faBolt, faShield, faPhone, faLocationDot, faPaperPlane, faCreditCard, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 /* Add icons to the library */
-library.add(faEnvelope, faHome, faHeart, faBook, faServer, faDatabase, faDiagramProject, faCode, faBolt, faShield, faGithub, faYoutube, faLinkedin, faPhone, faLocationDot, faPaperPlane, faCreditCard, faBars)
+library.add(faEnvelope, faHome, faHeart, faBook, faServer, faDatabase, faDiagramProject, faCode, faBolt, faShield, faGithub, faYoutube, faLinkedin, faPhone, faLocationDot, faPaperPlane, faCreditCard, faBars, faXmark)
 
 const app = createApp(App)
 
